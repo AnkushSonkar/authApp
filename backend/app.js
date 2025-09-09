22a021770fc586e309f7b119c98f045e76fa4dce
Ankush <ankushsonkar987@gmail.com>
2025-09-09 16:51:39 +0530

    removed vite express and implemented cors with render api

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,8 @@
 import express from "express";
 import dbConnection from "./config/config.js";
 import authRoute from "./routes/authRoute.js";
-import ViteExpress from "vite-express"; // your existing setup
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 app.use(cookieParser());
@@ -16,14 +16,12 @@ app.use(express.urlencoded({ extended: true }));
 // Use the signup router for POST /submit
 app.use("/", authRoute);
 
-ViteExpress.config({
-  inlineViteConfig: {
-    root: "../frontend",
-    base: "/",
-    build: { outDir: "dist" },
-  },
-});
+app.use(
+  cors({
+    origin: "https://authapp-1-d1d2.onrender.com",
+    credentials: true,
+  })
+);
 
-ViteExpress.listen(app, 3000, () => {
-  console.log("Listening on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API running on port ${PORT}`));
