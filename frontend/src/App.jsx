f3998ca088148065a46864b9d43c6a0c9cfb8597
Ankush <ankushsonkar987@gmail.com>
2025-09-09 20:54:06 +0530

    added protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,30 +4,21 @@ import { Routes, Route, Link, Outlet, Navigate } from "react-router-dom";
 import Home from "./component/Home";
 import Login from "./component/Login";
 import SignUp from "./component/SignUp";
-
-// function Layout() {
-//   return (
-//     <div>
-//       <nav className="p-4 bg-gray-200 flex justify-center gap-4 items-center">
-//         <Link to="/login" className="font-bold text-blue-700 text-2xl">
-//           Login
-//         </Link>
-//         <span className="font-bold">|</span>
-//         <Link to="/signup" className="font-bold text-blue-700 text-2xl">
-//           Sign Up
-//         </Link>
-//       </nav>
-//       <Outlet />
-//     </div>
-//   );
-// }
+import ProtectedRoute from "./component/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       {/* Redirect "/" → "/signup" */}
       <Route index element={<Navigate to="/signup" replace />} />
-      <Route path="home" element={<Home />} />
+      <Route
+        path="home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
       <Route path="login" element={<Login />} />
       <Route path="signup" element={<SignUp />} />
     </Routes>
