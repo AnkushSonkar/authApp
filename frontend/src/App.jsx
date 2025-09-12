1ec9628e06e198e29900dfb3c61ff6093ee6e784
Ankush <ankushsonkar987@gmail.com>
2025-09-12 22:29:40 +0530

    removed protected routes file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,13 @@ import { Routes, Route, Link, Outlet, Navigate } from "react-router-dom";
 import Home from "./component/Home";
 import Login from "./component/Login";
 import SignUp from "./component/SignUp";
-import ProtectedRoute from "./component/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       {/* Redirect "/" → "/signup" */}
       <Route index element={<Navigate to="/signup" replace />} />
-      <Route
-        path="home"
-        element={
-          // <ProtectedRoute>
-          <Home />
-          // </ProtectedRoute>
-        }
-      />
+      <Route path="home" element={<Home />} />
       <Route path="login" element={<Login />} />
       <Route path="signup" element={<SignUp />} />
     </Routes>
